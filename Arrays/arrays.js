c296a80c86604f0342e6911bfcfa28fa43a97475
Dilan <dilandhakal889@gmail.com>
2025-04-29 20:43:46 +0545

    refuse code

---
+++ Arrays/arrays.js
@@ -0,0 +1,20 @@
+//xreate array 
+
+let heroes = ["Ironman","hulk","spiderman"];
+
+let marks = [87 , 99 , 88 , 77 , 22];
+
+console.log(heroes);
+console.log(heroes.length);
+
+console.log(marks);
+console.log(marks.length);
+
+console.log(typeof marks);
+console.log(typeof heroes);
+
+
+console.log('1.' + heroes[0]);
+
+heroes[0] = "Wonder women";
+console.log(heroes);
