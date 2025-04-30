65b32cf8a6f2d87742fc34d19bb8dfa586b10075
Dilan <dilandhakal889@gmail.com>
2025-04-30 20:12:40 +0545

    add code

---
+++ Function and method/index.js
@@ -0,0 +1,48 @@
+console.log("Hello World");
+
+// function defination
+// function functionName(){
+//  do some work
+// }
+
+
+
+// function call
+// functionName();
+
+
+
+// tryout
+
+function myFunciton(){
+    console.log("I hate javaScript");
+}
+
+myFunciton();
+
+
+
+function showMessage(msg){  //msg is  paremeter ->
+    console.log(" ");
+    console.log(msg)
+}
+
+showMessage("My name is Zani"); //argumrnt
+
+
+
+function sumOfNumber(x,y){
+    console.log("Sum=" + x * y);
+}
+
+sumOfNumber(6,5)
+
+
+
+function nameOfGame(){
+    return "Elden ring";
+}
+
+let value = nameOfGame();
+console.log(value);
+
