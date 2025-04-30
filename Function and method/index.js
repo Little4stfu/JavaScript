18002f1ec80b79cc605f19297808bb60b9bc4a0d
Dilan <dilandhakal889@gmail.com>
2025-04-30 20:44:17 +0545

    add code

--- Function and method/index.js
+++ Function and method/index.js
@@ -31,7 +31,7 @@ showMessage("My name is Zani"); //argumrnt
 
 
 
-function sumOfNumber(x,y){
+function sumOfNumber(x,y){  //x and y is local variable
     console.log("Sum=" + x * y);
 }
 
@@ -46,3 +46,72 @@ function nameOfGame(){
 let value = nameOfGame();
 console.log(value);
 
+
+// Arrow function
+
+function sumofNum(a, b){
+    return a+b;
+}
+function multiplicationOfNum(a,b){
+    return a*b;
+}
+
+const sum = (a,b)=>{
+    console.log(a+b);
+}
+
+console.log(sum);
+sum(5,6);
+
+
+
+
+const multiplication = (a,b)=>{
+    console.log(a*b);
+}
+
+multiplication(6,5);
+
+
+
+const notification = ()=>{
+    console.log("Time to work");
+}
+
+notification();
+
+
+// create a function using the function keyword that takes a streing as an argumrnt and return numbers of vowels in the string 
+
+// create an arrow function to perform the same task.
+
+
+
+
+const aString = "anime";
+let count = 0;
+function vowel(a){
+    for(let i = 0; i<a.length; i++){
+        if(a[i] == 'a' || a[i] == 'e'|| a[i]=='i' || a[i] == 'o' || a[i] == 'u'){
+           count = count + 1;
+        }
+    }
+    return count;
+}
+
+console.log(vowel(aString));
+
+
+let Sum=0;
+const checkVowel = (a)=>{
+    for(let i = 0; i<a.length; i++){
+        
+        if(a[i] == 'a' || a[i] == 'e'|| a[i]=='i' || a[i] == 'o' || a[i] == 'u'){
+           Sum = Sum + 1;
+        }
+    }
+    return Sum;
+
+}
+
+console.log(checkVowel('amelia'));
