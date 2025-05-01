a4aac35217991a9e82935293971b23f7b0dc4b56
Dilan <dilandhakal889@gmail.com>
2025-05-01 20:28:27 +0545

    add code

---
+++ Dom/index.js
@@ -0,0 +1,23 @@
+console.log("Js connected");
+
+
+
+// window objects
+// the window object represent an open winbdow in browser. it is browser object (not javascript) and automatically created by browser
+
+// it is a global object with lots of properties and methods
+
+console.log(window);
+window.console.log("hello");
+
+
+
+// DOM
+// When a web page is loaded, the browser creates a document object model of the page
+
+// console.log -> this only print 
+// console.dir -> it print document properties and methods
+
+console.dir(window);
+console.log(window.document.body);
+
