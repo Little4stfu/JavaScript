96da26dae91467d43035304783e8563a95b1a20c
Dilan <dilandhakal889@gmail.com>
2025-05-06 21:22:09 +0545

    add code

--- eventHandlerShort/index.js
+++ eventHandlerShort/index.js
@@ -23,12 +23,54 @@ button.addEventListener('click',showMessage);
 
 let div2 = document.querySelector(".click");
 
-div2.onclick = (e) =>{
-    console.log(e);
-    console.log(event.type);
-    console.log(event.target);
-    console.log(event.clientX , event.clientY);
-}
+
 
 
 // EventListerners
+
+div2.addEventListener("click", (evt)=> {
+    console.log("Btn click was click");
+    console.log(evt);
+    console.log(evt.target);
+    console.log(evt.type);
+})
+
+
+div2.addEventListener("click",()=>{
+    console.log("handler 1");
+})
+
+div2.addEventListener("click",()=>{
+    console.log("handler 2");
+})
+
+
+div2.addEventListener("click",()=>{
+    console.log("handler 4");
+})
+
+const handler = () =>{
+    console.log("handler 3")
+}
+
+
+btn1.addEventListener("click",handler);
+btn1.removeEventListener("click",handler)
+
+
+
+
+// create a toggle button that change the screen to dark mode when clicked and light mode when clicked again.
+
+let modebtn = document.querySelector("#mode");
+let currMode = "light";
+
+modebtn.addEventListener("click", () =>{
+    if(currMode === "light"){
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    }else{
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+})
