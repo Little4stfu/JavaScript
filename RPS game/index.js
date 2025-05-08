31cba23c23cbebfd54d9b16c30221d893bbc9fc0
Dilan <dilandhakal889@gmail.com>
2025-05-08 20:33:44 +0545

    add code

--- RPS game/index.js
+++ RPS game/index.js
@@ -3,10 +3,25 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genComputerChoice = () =>{
+    const options = ["rock","paper","scissors"];
+    
+    //  rock,paper,scissor
+
+}
+
+
+const play = (userChoice) =>{
+    console.log("userChoice =",userChoice)
+    // generate computer choice
+}
+
+
 choices.forEach((choice)=>{
     choice.addEventListener("click",()=>{
         const userChoice = choice.getAttribute("id");
         console.log("clicked",userChoice);
+        playGame(userChoice);
     })
 })
 
