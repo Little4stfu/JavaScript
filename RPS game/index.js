8da6976cf7c1273d46d617feffe5bacc43549eba
Dilan <dilandhakal889@gmail.com>
2025-05-08 20:49:48 +0545

    add code

--- RPS game/index.js
+++ RPS game/index.js
@@ -2,25 +2,62 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
 const genComputerChoice = () =>{
     const options = ["rock","paper","scissors"];
-    
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
     //  rock,paper,scissor
+}
 
+const showWinner = (userWin)=>{
+    if(userWin){
+        console.log("You win");
+        msg.innerText = "User win";
+        msg.style.backgroundColor = "green"
+    }
+    else{
+        console.log("you lose");
+        msg.innerText = "you lose";
+        msg.style.backgroundColor = "red"
+    }
+}
+
+const drawGame = () =>{
+    console.log("Game was draw");
+    msg.innerText = "Game was draw";
+    msg.style.backgroundColor = "blue"
 }
 
 
-const play = (userChoice) =>{
+const playGame = (userChoice) =>{
     console.log("userChoice =",userChoice)
     // generate computer choice
+    const compScore = genComputerChoice();
+    console.log("comp choice =",compScore)
+
+    if(userChoice === compScore){
+        drawGame();
+    }
+    else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compScore === "paper" ? false: true;
+        } else if(userChoice === "paper"){
+            userWin === "scissors" ? false : true;
+        } else {
+            userWin = compScore === "rock" ? false : true;
+        }
+        showWinner(userWin);
+    }
+
 }
 
 
 choices.forEach((choice)=>{
     choice.addEventListener("click",()=>{
         const userChoice = choice.getAttribute("id");
-        console.log("clicked",userChoice);
         playGame(userChoice);
     })
 })
